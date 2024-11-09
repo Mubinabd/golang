c56684f5aeb1f5b7fcab95d349f76a4b1275d21e
Mubina Bahodirova <bahodirovamubina505@gmail.com>
2024-11-09 12:30:42 +0500

    ;

---
+++ C.go
@@ -0,0 +1,158 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var num int
+	// fmt.Scan(&num)
+
+	// res := num * 5
+	// fmt.Println(res)
+
+
+	// var a,b,c int
+	// fmt.Scan(&a)
+	// fmt.Scan(&b)
+	// fmt.Scan(&c)
+
+	// S := 2*(a*b + a*c + b*c)
+	// fmt.Println(S)
+
+	// var num int
+	// fmt.Scan(&num)
+	// res := 6*(num*num)
+	// fmt.Println(res)
+
+
+	// var num int
+	// fmt.Scan(&num)
+	// res := num*num*num
+	// fmt.Println(res)
+
+	// var a,b,c float32
+	// fmt.Scan(&a)
+	// fmt.Scan(&b)
+	// fmt.Scan(&c)
+
+	// orta := (a+b+c)/3
+	// fmt.Println(orta)
+
+
+	// var num int
+
+	// fmt.Scan(&num)
+
+	// a := num / 100
+
+	// fmt.Println("yuzlar xonasidagi son:",a)
+
+	// c := (num / 10) % 10
+	// fmt.Println("onlar xonasidagi son: ",c)
+
+	// b := num % 10
+	// fmt.Println("birlar xonasidagi son: ",b)
+
+
+	// var num int
+	// fmt.Scan(&num)
+
+	// res := num / 12
+	// fmt.Println(res ,"yil")
+
+	// var a,b int
+	// fmt.Scan(&a)
+	// fmt.Scan(&b)
+
+	// a,b = b,a
+
+	// fmt.Println("a son",a)
+	// fmt.Println("b son",b)
+
+	// var num int
+	// fmt.Scan(&num)
+
+	// if num %3 == 0 && num %6 == 0{
+	// 	fmt.Println("good")
+	// }else{
+	// 	fmt.Println("bad")
+	// }
+
+	// var a,b int
+	// fmt.Scan(&a)
+	// fmt.Scan(&b)
+
+	// if a % b == 0{
+	// 	fmt.Println("chelsea")
+	// }else{
+	// 	fmt.Println("liverpool")
+	// }
+
+	// var a int
+	// fmt.Scan(&a)
+
+	// if a < 0{
+	// 	a := -a
+	// 	fmt.Println(a)
+	// }else{
+	// 	fmt.Println(a)
+	// }
+
+	// var num int
+	// fmt.Scan(&num)
+
+	// switch num {
+	// 	case 1:{
+	// 		fmt.Println("Monday")
+	// 	}
+	// 	case 2:{	
+	// 		fmt.Println("Tuesday")
+	// 	}
+	// 	case 3:{
+	// 		fmt.Println("Wednesday")
+	// 	}
+	// 	case 4:{
+	// 		fmt.Println("Thursday")
+	// 	}
+	// 	case 5:{
+	// 		fmt.Println("Friday")
+	// 	}
+	// 	case 6:{
+	// 		fmt.Println("Saturday")
+	// 	}
+	// 	case 7:{
+	// 		fmt.Println("Sunday")
+	// 	}
+
+	// 	default:{
+	// 		fmt.Println("BUnday kun yoq")
+	// 	}
+	// }
+
+
+	// var a, b int
+	// var belgi string
+
+	// fmt.Print("Enter first number: ")
+	// fmt.Scan(&a)
+	// fmt.Print("Enter second number: ")
+	// fmt.Scan(&b)
+	// fmt.Print("Enter operator (+, -, *, /): ")
+	// fmt.Scan(&belgi)
+
+	// switch belgi {
+	// case "+":
+	// 	fmt.Println("Result:", a+b)
+	// case "-":
+	// 	fmt.Println("Result:", a-b)
+	// case "*":
+	// 	fmt.Println("Result:", a*b)
+	// case "/":
+	// 	if b != 0 {
+	// 		fmt.Println("Result:", a/b)
+	// 	} else {
+	// 		fmt.Println("Error: Division by zero is undefined")
+	// 	}
+	// default:
+	// 	fmt.Println("Error: Invalid operator")
+	// }
+}
